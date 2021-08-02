f4e0b0340bc20fa88850ec0021258caffef01787
Seob <61955818+seobkim@users.noreply.github.com>
2021-08-02 15:56:50 +0900

    Update Custom_Vid_control.js

--- Custom_Vid_control.js
+++ Custom_Vid_control.js
@@ -4,16 +4,17 @@ function speedbtn(){
 		console.log(video.playbackRate);
 	}
 	
-	function RewindVideo(){
-		video = document.getElementById("video");
-		intervalRewind = setInterval(function(){
-		       video.playbackRate = 1.0;
-		       if(video.currentTime == 0){
-		           clearInterval(intervalRewind);
-		           video.pause();
-		       }
-		       else{
-		           video.currentTime += -.1;
-		       }
-		                },30);
-	}
+function RewindVideo(){
+	video = document.getElementById("video");
+	intervalRewind = setInterval(function(){
+		video.playbackRate = 1.0;
+		if(video.currentTime == 0){
+			clearInterval(intervalRewind);
+			video.pause();
+		}
+		else{
+			video.currentTime += -.1;
+		}
+	},30);
+	
+}
