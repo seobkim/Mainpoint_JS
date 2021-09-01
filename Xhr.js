129d670a15195c8ef782590eb4b46756d84e81d8
Seob <61955818+seobkim@users.noreply.github.com>
2021-09-01 11:05:31 +0900

    Create Xhr.js

---
+++ Xhr.js
@@ -0,0 +1,7 @@
+var xhr = new XMLHttpRequest();
+xhr.open('POST', url, true);
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhr.onload = function(e) {
+		
+	}
+xhr.send(param);
