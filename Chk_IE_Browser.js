c581fb36919dbf17f951001d71f994629fa3c9cf
Seob <61955818+seobkim@users.noreply.github.com>
2021-11-15 09:56:42 +0900

    Create Chk_IE_Browser.js

---
+++ Chk_IE_Browser.js
@@ -0,0 +1,6 @@
+if ( (navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
+						     alert("IE 브라우저에서 지원하지 않는 기능입니다.")
+		}
+else{
+  //TODO IE 아닐 시 수행 코드
+}
