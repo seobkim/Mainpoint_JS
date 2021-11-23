5d3fa9cd5a06e72074629e777cdab9b44eefde85
Seob <61955818+seobkim@users.noreply.github.com>
2021-11-23 09:30:31 +0900

    Create object_key_values.js

---
+++ object_key_values.js
@@ -0,0 +1,12 @@
+var objectVar = {"A": 1 , "B": 2, "C": 3};
+
+//key 값 / 길이 가져오기
+
+//길이
+var lengthTemp = Object.keys(objectVar).length;
+
+//키값
+var keyTemp = var temp = Object.keys(objectVar);
+
+//값
+var valueTemp = Object.values(objectVar);
