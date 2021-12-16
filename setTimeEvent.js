5882198428ed571102070175e124736304da1401
Seob <61955818+seobkim@users.noreply.github.com>
2021-12-16 09:48:02 +0900

    Create setTimeEvent.js

---
+++ setTimeEvent.js
@@ -0,0 +1,8 @@
+//setTimeout을 이용해 일정 시간이 지난 후에 함수를 실행하는 방법
+clearTimeout(timerId);
+
+//setInterval을 이용해 일정 시간 간격을 두고 함수를 실행하는 방법
+
+//setTimeout이 함수를 단 한 번만 실행하는 것과 달리 setInterval은 함수를 주기적으로 실행
+//함수 호출을 중단하려면 
+clearInterval(timerId)
